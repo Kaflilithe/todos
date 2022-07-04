df582365deab8de04a1a5941f14e5f5121177a6c
daraivanova <kudiyarova.1975@gmail.com>
2022-07-04 04:18:17 +0300

    Добавлен Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TodosComponent } from './todos/todos.component';
 import { Config } from './core/config';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AlbumsComponent } from './albums/albums.component';
@@ -11,16 +10,17 @@ import { MainComponent } from './main/main.component';
 import { UserSelectComponent } from './user-select/user-select.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { TodoItemComponent } from './todo-item/todo-item.component';
+import { HeaderComponent } from './shared/components/core/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    TodosComponent,
     AlbumsComponent,
     UserSelectComponent,
     TodoListComponent,
     TodoItemComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
