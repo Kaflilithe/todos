50b1241e71ae2dc9d3e801d74e83bcd73845e2d3
daraivanova <kudiyarova.1975@gmail.com>
2022-07-05 22:21:12 +0300

    Создан компонент Albums

---
+++ src/app/albums/albums.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { AlbumDto} from '../core/todos.service';
+
+@Component({
+  selector: 'app-albums',
+  templateUrl: './albums.component.html',
+  styleUrls: ['./albums.component.scss']
+})
+export class AlbumsComponent implements OnInit {
+  @Input()
+  album: AlbumDto = {
+    'userId': 0,
+    'id': 0,
+    'title': '',
+  };
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
